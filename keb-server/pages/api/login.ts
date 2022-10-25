f7cc0fbbd1f022d905ad5a4ad64a8b512ccfb092
Flavio Coutinho <coutinhoflavio20@gmail.com>
2022-10-25 12:24:06 -0300

    Initial commit

---
+++ keb-server/pages/api/login.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default (
+    req : NextApiRequest,
+    res : NextApiResponse
+) => {
+    if(req.method === 'POST'){
+        const {login, senha} = req.body;
+
+        if(login === "admin@admin.com" && senha === "Admin123"){
+            res.status(200).json({msg : "Usuaário autenticado com sucesso"});
+        }
+        return res.status(405).json({erro : 'Usuário ou senha não encontradas'});
+    }
+    return res.status(405).json({erro : "Método informado não é válido"});
+}
